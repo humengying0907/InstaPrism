9a6498b0c1dbcc89b5e51a93f5b1448b2ac9f1cc
Mengying Hu <humengying0907@mengyings-mbp.home>
2024-06-03 00:57:32 -0400

    update DESCRIPTION

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' @description a simulated dataset containing  simulate bulk and single-cell expression,
 #' as well as marker genes and true proportions that can be used as an example for deconvolution.
-#'
+#' @import Biobase
 #' @example
 #' data(sim.data)
 "sim.data"
